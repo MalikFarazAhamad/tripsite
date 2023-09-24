97c8b1a2fff37c3c0ff72d0b09da6ad0d01d27fa
Faraz Ahmad <145370270+MalikFarazAhamad@users.noreply.github.com>
2023-09-23 23:48:10 -0700

    Add files via upload

---
+++ tripsite/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def Services(request):
+    return render(request,'Services.html')
+def Gallery(request):
+    return render(request,'Gallery.html')
+    

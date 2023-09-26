870de5e3d83192bf11fba12061cf8383ed1e6102
Faraz Ahmad <145370270+MalikFarazAhamad@users.noreply.github.com>
2023-09-25 22:08:26 -0700

    Update views.py
    add the backend logic of contact.html in views.py

--- tripsite/home/views.py
+++ tripsite/home/views.py
@@ -11,5 +11,13 @@ def Services(request):
     return render(request,'Services.html')
 def Gallery(request):
     return render(request,'Gallery.html')
-def Contact(request):
-    return render(request, 'Contact.html')
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
